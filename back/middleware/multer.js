4b0ed84f803241139fd570d574fdbf625c993535
gwenaelgressier <97903290+gwenaelgressier@users.noreply.github.com>
2022-06-17 12:54:51 +0200

    petite modif

--- back/middleware/multer.js
+++ back/middleware/multer.js
@@ -1,11 +1,5 @@
 const multer = require("multer");
 
-const MIME_TYPES = {
-    "image/jpg": "jpg",
-    "image/jpeg": "jpeg",
-    "image/png": "png",
-};
-
 const storage = multer.diskStorage({
     destination: "images/",
     filename: function (req, file, cb) {
